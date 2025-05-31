df0374e57202ed8f577e42d3f16cbade42e89b56
claarotinha <clar4costa705@gmail.com>
2025-05-31 20:28:17 -0300

    eba

--- My project (1)/Assets/Settings/scripts/flipper.cs
+++ My project (1)/Assets/Settings/scripts/flipper.cs
@@ -1,33 +1,29 @@
 
 using UnityEngine;
 
-public class FlipperController : MonoBehaviour
+public class Flipper : MonoBehaviour
 {
-    public KeyCode activationKey = KeyCode.LeftArrow;
+    public KeyCode key;
     private HingeJoint2D hinge;
 
     void Start()
     {
         hinge = GetComponent<HingeJoint2D>();
-        hinge.useMotor = true;
-        hinge.useLimits = true;
-
-        JointAngleLimits2D limits = new JointAngleLimits2D();
-        limits.min = -45f;
-        limits.max = 45f;
-        hinge.limits = limits;
     }
 
     void Update()
     {
         JointMotor2D motor = hinge.motor;
 
-        if (Input.GetKey(activationKey))
-            motor.motorSpeed = -1000f;
+        if (Input.GetKey(key))
+        {
+            motor.motorSpeed = -1000f; // Teste com positivo/negativo
+        }
         else
+        {
             motor.motorSpeed = 1000f;
+        }
 
-        motor.maxMotorTorque = 10000f;
         hinge.motor = motor;
     }
 }
